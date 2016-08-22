95c216a361dec8b226ee4eaf576d2984c4011641
Daniel St. Clair <stclair.daniel@gmail.com>
2016-08-21 18:28:57 -0700

    completed basic logic and styling

---
+++ solution/game.js
@@ -0,0 +1,32 @@
+const Board = require("./board");
+const MoveError = require("./moveError");
+
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.currentPlayer = Board.marks[0];
+  }
+
+  isOver() {
+    return this.board.isOver();
+  }
+
+  playMove(pos) {
+    this.board.placeMark(pos, this.currentPlayer);
+    this.swapTurn();
+  }
+
+  swapTurn() {
+    if (this.currentPlayer === Board.marks[0]) {
+      this.currentPlayer = Board.marks[1];
+    } else {
+      this.currentPlayer = Board.marks[0];
+    }
+  }
+
+  winner() {
+    return this.board.winner();
+  }
+}
+
+module.exports = Game;
